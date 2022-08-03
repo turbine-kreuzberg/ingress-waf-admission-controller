6078e8d060acd92c509fd8be26ef0a11588a1ad7
Adam Hukalowicz <adam.hukalowicz@turbinekreuzberg.com>
2022-08-03 14:22:30 +0200

    [FIX] - dump anomaly score and threshold if the anomaly score is greater than 0

--- mutatingwebhook.go
+++ mutatingwebhook.go
@@ -89,7 +89,7 @@ SecAuditLogRelevantStatus 403
 SecAuditLog /dev/stdout
 SecAuditLogParts ABFHZ
 SecAction "id:900110,phase:1,log,pass,t:none,setvar:tx.inbound_anomaly_score_threshold=%s"
-SecAction "phase:5,auditlog,log,pass,msg:\'Anomaly Score %%{TX.anomaly_score} Threshold %%{TX.inbound_anomaly_score_threshold}\'"
+SecRule TX:ANOMALY_SCORE "@gt 0" "id:10001,phase:5,auditlog,log,pass,msg:\'Anomaly Score %%{TX.anomaly_score} Threshold %%{TX.inbound_anomaly_score_threshold}\'"
 %s
 `, threshold, additionalCRS)
 
